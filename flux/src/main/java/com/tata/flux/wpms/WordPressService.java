c2c6e9cbc8a1fe674a0f6533c2ef9343ea51a884
Christian Ibarra <hola@weeks2.com>
2023-05-04 19:56:19 -0600

    (R) wp-api: refactor service layer.

--- flux/src/main/java/com/tata/flux/wpms/WordPressService.java
+++ flux/src/main/java/com/tata/flux/wpms/WordPressService.java
@@ -1,12 +1,18 @@
 package com.tata.flux.wpms;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tata.flux.model.SitiosWeb;
 import com.tata.flux.service.FtpUtility;
-import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -19,6 +25,7 @@ import java.util.Arrays;
 public class WordPressService {
     private final WebClient webClient;
     private final FtpUtility ftpUtility;
+    private static final String WORDPRESS_API_URL = "https://your-wordpress-site.com/wp-json/wp/v2/posts";
     public WordPressService(WebClient.Builder webClientBuilder, FtpUtility ftpUtility) {
         this.webClient = webClientBuilder.build();
         this.ftpUtility = ftpUtility;
@@ -64,7 +71,7 @@ public class WordPressService {
 
     void init() {
         String headerUri = createUri("1").replace("PAGE","1");
-        header(headerUri).map(totalPages->{
+        readheader("x-wp-totalpages",headerUri).map(totalPages->{
             Flux.range(1,totalPages)
                     .concatMap(page-> {
                         try {
@@ -92,29 +99,23 @@ public class WordPressService {
         }).subscribe();
     }
 
-    /**
-     * X-WP-Total: 17454
-     * X-WP-TotalPages: 175
-     */
-    public Mono<Integer> header(String uri) {
+    public Mono<Integer> readheader(String header,String uri) {
         return webClient.get().uri(uri).exchangeToMono(response -> response.toBodilessEntity())
-                .map(entity -> entity.getHeaders().getFirst("x-wp-totalpages"))
+                .map(entity -> entity.getHeaders().getFirst(header))
                 .map(totalPagesStr -> totalPagesStr != null ? Integer.parseInt(totalPagesStr) : 0);
     }
 
     private Flux<Post> pullPosts(String uri) {
         log.info("site {}",uri);
-        return webClient.get().uri(uri)
-                .retrieve().bodyToFlux(Post.class)
-                .doOnNext(d-> {
-                   //log.info("{}",d.getTitle());
-                });
+        return webClient.get().uri(uri).retrieve().bodyToFlux(Post.class)
+                .doOnNext(d-> log.info("{}",d.getTitle()));
     }
 
-    
-    private Flux<Post> pullAll(String number) 
-    {  
-        Flux<Post> posts = header(SitiosWeb.getLetter(number)).flatMapMany(totalPages-> {
+    /**
+     * X-WP-Total: 17454, X-WP-TotalPages: 175
+     */
+    private Flux<Post> pullAll(String number) {
+        Flux<Post> posts = readheader("x-wp-totalpages",SitiosWeb.getLetter(number)).flatMapMany(totalPages-> {
             return Flux.range(1,totalPages).concatMap(page-> {
                        return pullPosts(SitiosWeb.getLetterPage(number).replace("PAGE", page.toString()));
                     });
@@ -134,8 +135,44 @@ public class WordPressService {
                     catch (Exception e) {
                         return Flux.error(e);
                     }
-        },5).onErrorContinue((t,o) -> {
+        },5)
+        .onErrorContinue((t,o) -> {
             log.error("{}", t.getMessage());
         }).subscribe();
     }
+ public static void testApi() {
+        WebClient webClient = WebClient.builder().baseUrl(WORDPRESS_API_URL).build();
+
+        webClient.get().accept(MediaType.APPLICATION_JSON).retrieve()
+        .bodyToFlux(JsonNode.class)
+                .map(postNode -> {
+                    String title = postNode.get("title").get("rendered").asText();
+                    String author = postNode.get("author").asText();
+                    String content = postNode.get("content").get("rendered").asText();
+                    String date = postNode.get("date").asText();
+                    // Agregue cualquier otra propiedad de la publicación que desee acceder aquí
+                    return String.format("Title: %s, Author: %s, Content: %s, Date: %s", title, author, content, date);
+                })
+                .subscribe(System.out::println);
+    }
+
+    public static void testApi_(String[] args) {
+        WebClient webClient = WebClient.builder().baseUrl(WORDPRESS_API_URL).build();
+
+        MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+        data.add("title", "Title of the new post");
+        data.add("content", "Content of the new post");
+        data.add("status", "publish");
+
+        webClient.post()
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(data))
+                .header(HttpHeaders.AUTHORIZATION, "Bearer YOUR_ACCESS_TOKEN")
+                .retrieve()
+                .bodyToMono(JsonNode.class)
+                .subscribe(response -> {
+                    System.out.println("New post ID: " + response.get("id").asInt());
+                });
+            }
+
 }
